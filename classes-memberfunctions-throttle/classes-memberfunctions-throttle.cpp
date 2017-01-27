643e584ef8704cc03231f891208936143932e1b8
leer89 <leer89@gmail.com>
2017-01-26 20:47:08 -0500

    reverted back to old version with no new new constructor / top position private variable

--- classes-memberfunctions-throttle/classes-memberfunctions-throttle.cpp
+++ classes-memberfunctions-throttle/classes-memberfunctions-throttle.cpp
@@ -1,103 +1,82 @@
-// classes-memberfunctions-throttle.cpp : Defines the entry point for the console application.
-//
+// Throttle Class
+// This small demonstration shows how the throttle class is used.
 
 #include "stdafx.h"
 
-#include <iostream>		// provides cout and cin
-#include <cstdlib>
-#include <cassert>
-// provides exit_success
-using namespace std;	// allows all standard library items ot be used
+#include <iostream>         // Provides cout and cin
+#include <cstdlib>          // Provides EXIT_SUCCESS
+using namespace std;        // Allows all Standard Library items to be used
 
-// this data type is a class
 class throttle
 {
-	// member list
-	public:
-		// CONSTRUCTORS
-		throttle();
-		throttle(int size)
-		// MODIFICATION MEMBER FUNCTIONS
-		void shut_off() { position = 0; }
-		void shift(int amount);
-
-		// CONSTANT MEMBER FUNCTIONS
-		double flow() const { return position / double(top_position); }
-		bool is_on() const { return (position < 0); }
-
-	private:
-		int top_position;
-		int position;
+public:
+	// MODIFICATION MEMBER FUNCTIONS
+	void shut_off();
+	void shift(int amount);
+	// CONSTANT MEMBER FUNCTIONS
+	double flow() const;
+	bool is_on() const;
+private:
+	int position;
 };
-
-throttle::throttle()
-{
-	top_position = 1;
-	position = 0;
-}
-
-throttle::throttle(int size)
-// library facilities used: cassert
-{
-	assert(0 > size);
-	top_position = size;
-	position = 0;
-}
 
 int main()
 {
 	throttle sample;
 	int user_input;
-
-	// set the sample throttle to a position indicated by the user.
+	// Set the sample throttle to a position indicated by the user.
 	cout << "I have a throttle with 6 positions." << endl;
 	cout << "Where would you like to set the throttle? " << endl;
-	cout << "Please type a number form 0 to 6: ";
+	cout << "Please type a number from 0 to 6: ";
 	cin >> user_input;
 	sample.shut_off();
 	sample.shift(user_input);
-
-	// shift the throttle downt o zero, printing the flow along thew way.
+	// Shift the throttle down to zero, printing the flow along the way.
 	while (sample.is_on())
 	{
 		cout << "The flow is now " << sample.flow() << endl;
 		sample.shift(-1);
 	}
-	cout << "The flow is now " << sample.flow() << endl;
+	cout << "The flow is now off" << endl;
 	return EXIT_SUCCESS;
 }
 
 void throttle::shut_off()
-// precondition: none
-// postcondition: the throttle has been turned off
+// Precondition: None.
+// Postcondition: The throttle has been turned off.
 {
 	position = 0;
 }
 
-void throttle::shift(int amount)
-// precondition: shut_off has been called at least once to initialize the throttle.
-// postcoondition: the throttle's position has been moved by amount (but
-// not below 0 or above 6)
-{
-	position += amount;
-	if (position < 0)
-		position = 0;
-	else if (position > top_position)
-		position = top_position;
-}
-
 double throttle::flow() const
-// precondition: shut_off has been called at least once to initialize the throttle
-// postcondition: the value returned is the current flow as a proportion
-// of maximum flow
+// Precondition: shut_off has been called at least once to initialize the
+// throttle.
+// Postcondition: The value returned is the current flow as a proportion of
+// the maximum flow.
 {
 	return position / 6.0;
 }
 
-bool throttle::is_on() const
-// precondition : shut_off has been called at least once to initialize the throttle
-// postcondition: if the throttle's flow is above 0, then the function
-// returns true; otherwise, it returns false.
+void throttle::shift(int amount)
+// Precondition: shut_off has been called at least once to initialize the
+// throttle.
+// Postcondition: The throttle's position has been moved by amount (but not
+// below 0 or above 6).
 {
-	return(flow() > 0);
+	position += amount;
+
+	// Adjust the throttle if it went too high or too low
+	if (position < 0)
+		position = 0;
+	else if (position > 6)
+		position = 6;
+}
+
+bool throttle::is_on() const
+// Precondition: shut_off has been called at least once to initialize the
+// throttle.
+// Postcondition: If the throttle's flow is above 0 then the function
+// returns true; otherwise it returns false.
+{
+	return (flow() > 0);
 }
